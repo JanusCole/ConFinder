eac1d52d0b9888c358a9b12245b2c6ba803c15b8
janus <januscole@yahoo.com>
2018-04-10 13:01:01 -0400

    Fixed the ConventionsWebAPI unit test

--- app/src/test/java/com/example/janus/confinder/ConventionsWebAPIUnitTest.java
+++ app/src/test/java/com/example/janus/confinder/ConventionsWebAPIUnitTest.java
@@ -1,6 +1,7 @@
 package com.example.janus.confinder;
 
 import com.example.janus.confinder.data.Convention;
+import com.example.janus.confinder.data.ConventionsService;
 import com.example.janus.confinder.data.ConventionsWebAPI;
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
@@ -8,13 +9,15 @@ import com.squareup.okhttp.mockwebserver.MockWebServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class RetrofitUnitTest {
+public class ConventionsWebAPIUnitTest {
 
     String retroFiteResponse = "[{\"@context\":\"http://schema.org/\",\"@type\":\"Event\",\"name\":\"Ikkicon\",\"startDate\":\"2018-12-29\",\"endDate\":\"2018-12-31\",\"url\":\"http://fancons.com/events/info.shtml/9192/Ikkicon_2018/\",\"location\":{\"@type\":\"Place\",\"name\":\"Renaissance Austin Hotel\",\"address\":{\"@type\":\"PostalAddress\",\"addressLocality\":\"Austin\",\"addressRegion\":\"TX\",\"postalCode\":\"78759\",\"addressCountry\":\"USA\"}}}]";
 
@@ -32,9 +35,18 @@ public class RetrofitUnitTest {
 
         ConventionsWebAPI retrofitConsFromWeb = new ConventionsWebAPI();
         retrofitConsFromWeb.setBaseURL(mockWebServer.url("").toString());
-        List<Convention> conventionsFromWeb = retrofitConsFromWeb.getConventions();
 
-        assertEquals(null, conventionsFromWeb);
+        retrofitConsFromWeb.getConventions(new ConventionsService.ConventionsDataSourceCallback() {
+            @Override
+            public void onConventionsComplete(List<Convention> conventions) {
+                assertTrue(false);
+            }
+
+            @Override
+            public void onNetworkError() {
+                assertTrue(true);
+            }
+        });
 
     }
 
@@ -45,14 +57,27 @@ public class RetrofitUnitTest {
 
         ConventionsWebAPI retrofitConsFromWeb = new ConventionsWebAPI();
         retrofitConsFromWeb.setBaseURL(mockWebServer.url("").toString());
-        List<Convention> conventionsFromWeb = retrofitConsFromWeb.getConventions();
-        Convention oneConvention = conventionsFromWeb.get(0);
+        retrofitConsFromWeb.getConventions(new ConventionsService.ConventionsDataSourceCallback() {
+            @Override
+            public void onConventionsComplete(List<Convention> conventions) {
+                Convention oneConvention = conventions.get(0);
 
-        assertFalse(oneConvention == null);
-        assertEquals("Ikkicon", oneConvention.getName());
-        assertEquals("2018-12-29", oneConvention.getStartDate());
-        assertEquals("Austin, USA", oneConvention.getAddress());
-        assertEquals("http://fancons.com/events/info.shtml/9192/Ikkicon_2018/", oneConvention.getWebsite());
+                assertFalse(oneConvention == null);
+                assertEquals("Ikkicon", oneConvention.getName());
+                assertEquals("2018-12-29", oneConvention.getStartDate());
+                assertEquals("Austin, USA", oneConvention.getAddress());
+                assertEquals("http://fancons.com/events/info.shtml/9192/Ikkicon_2018/", oneConvention.getWebsite());
+
+            }
+
+            @Override
+            public void onNetworkError() {
+                assertTrue(false);
+
+            }
+        });
+
+
 
     }
 
